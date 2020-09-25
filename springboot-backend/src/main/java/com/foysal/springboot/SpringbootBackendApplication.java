f6cf245587ce39707d0a00abe58200ad770ec005
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-09-25 10:41:13 +0600

    Spring boot backend files add

---
+++ springboot-backend/src/main/java/com/foysal/springboot/SpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.foysal.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendApplication.class, args);
+	}
+
+}
